fef5e51dfd84c3d19b38d0aa3895b2a083baddb2
Hüseyin Emre Üğdül <drekipersonal@outlook.com>
2024-03-15 23:47:02 +0300

    MyProfile screen implemented

--- nurse_scheduling/src/pages/MyProfile.tsx
+++ nurse_scheduling/src/pages/MyProfile.tsx
@@ -1,15 +1,89 @@
+import {Avatar, Box, Center, HStack, VStack, Text, Input, Pressable, Icon} from 'native-base';
 import React from 'react';
-import {
-    Text, View,
+import { Icon as ThemedIcon} from '@rneui/themed';
 
-} from 'react-native';
+
 
 function MYProfile(): React.JSX.Element {
+    const nurse = {
+        firstName: "Hüseyin Emre",
+        lastName: "Üğdül",
+        birthDate: "4 Haziran 2000",
+        department: "Dahiliye",
+        profilePicture: "https://cdn-icons-png.flaticon.com/512/8496/8496122.png"
+    }
+    
 
     return (
-        <View>
-            <Text>My Profile Page</Text>
-        </View>
+        <Box>
+            <Center>
+                <HStack space={4} alignItems="center" mt="9" mb="9">
+                    <Box>
+                        <Avatar bg="#5F374B" source={{uri: nurse.profilePicture}} size="2xl" ></Avatar>
+                    </Box>
+                    <Box>
+                        <VStack>
+                            <Box>
+                                <Text fontSize="2xl">{nurse.firstName}</Text>
+                            </Box>
+                            <Box>
+                                <Text bold fontSize="2xl">{nurse.lastName}</Text>
+                            </Box>
+                        </VStack>
+                    </Box>
+                </HStack>
+            </Center>
+
+            <Box >
+                <VStack space="6"  >
+                    <HStack alignItems="center">
+                        <Text fontSize="md" ml="6">Departman    : </Text>
+                        <Input variant="outline" placeholder={nurse.department} isDisabled w={{
+                            base: "55%",
+                            md: "25%"
+                        }}/>
+                    </HStack>
+                    <HStack alignItems="center">
+                        <Text fontSize="md" ml="6">Doğum Tarihi: </Text>
+                        <Input variant="outline" placeholder={nurse.birthDate} isDisabled w={{
+                            base: "55%",
+                            md: "25%"
+                        }}/>
+                    </HStack>
+                </VStack>
+            </Box>
+
+            <Box ml="6" mt={12} w={{
+                base: "85%",
+                md: "25%"
+            }}>
+                <Pressable style={{backgroundColor: "gray"}} rounded="8">
+                    <HStack justifyContent="space-between" minHeight = "12" alignItems="center">
+                        <HStack alignItems="center" space={1}>
+                            <Icon as={<ThemedIcon name={"calendar-outline"} type="ionicon" />} size={6} ml="3"  />
+                            <Text style={{color: "white", textAlign: "center", fontSize: 20}}>Çalışma Takvimim</Text>
+                        </HStack>
+                        <Icon as={<ThemedIcon name={"chevron-forward-outline"} type="ionicon" />} size={6} mr="3"  />
+                    </HStack>
+                </Pressable>
+            </Box>
+
+            <Box  ml="6" mt={12} w={{
+                base: "85%",
+                md: "25%"
+            }}>
+                <Pressable style={{backgroundColor: "red"}} rounded="8">
+                    <HStack justifyContent="space-between" minHeight = "9" alignItems="center">
+                        <HStack alignItems="center" space={1}>
+                            <Icon as={<ThemedIcon name={"log-out-outline"} type="ionicon" />} size={6} ml="3"  />
+                            <Text style={{color: "white", textAlign: "center", fontSize: 20}}>Çıkış Yap</Text>
+                        </HStack>
+                        <Icon as={<ThemedIcon name={"chevron-forward-outline"} type="ionicon" />} size={6} mr="3" color='#fff' />
+                    </HStack>
+                </Pressable>
+
+            </Box>
+        </Box>
     );
 }
 
