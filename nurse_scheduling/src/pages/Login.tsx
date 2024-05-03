898e85e37412c387a77494bbf05921b164b5b71e
Mert Batuhan ÜNVERD <mert.batuhan.unverdi@saritay.com>
2024-05-04 02:44:21 +0300

    fix: Login page refactoring, added error Modal and Hint popover.

--- nurse_scheduling/src/pages/Login.tsx
+++ nurse_scheduling/src/pages/Login.tsx
@@ -1,9 +1,20 @@
-import {Avatar, Box, Center, Input, Icon, Pressable, Stack} from 'native-base';
+import {
+    Avatar,
+    Box,
+    Center,
+    Input,
+    Icon,
+    Pressable,
+    Stack,
+    Text,
+    Alert,
+    VStack,
+    HStack,
+    IconButton,
+    CloseIcon, Popover
+} from 'native-base';
 import React, {useState, useEffect, useContext} from 'react';
 import {Icon as ThemedIcon} from '@rneui/themed';
-import {
-    Text,
-} from 'react-native';
 import {AuthContext} from "../contexts/AuthContext.tsx";
 import {login} from "../apis/auth.tsx";
 import {Buffer} from "buffer";
@@ -14,8 +25,7 @@ function Login(): React.JSX.Element {
 
     const [tcno, setTcno] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [tcnoError, setTcnoError] = useState<string>("");
-    const [show, setShow] = React.useState(false);
+    const [showPassword, setShowPasswordPassword] = React.useState(false);
     const [disabled, setDisabled] = React.useState(true);
     const [errorMessage, setErrorMessage] = useState<string>("");
     const {setIsAuth} = useContext(AuthContext);
@@ -23,13 +33,11 @@ function Login(): React.JSX.Element {
     const handleLoginSubmit = async () => {
         if(tcno.length === 0 || password.length === 0) {
             setErrorMessage("Lütfen tüm alanları doldurunuz.");
-            console.error("Login error:", "Lütfen tüm alanları doldurunuz.");
             return;
         }
         let response = await login(tcno, password) as NurseType;
         if(response && response.errorMessage !== null) {
             setErrorMessage(response.errorMessage);
-            console.error("Login error:", response.errorMessage);
             return;
         }
 
@@ -42,18 +50,60 @@ function Login(): React.JSX.Element {
     };
 
     useEffect(() => {
-        if (tcno?.length !== 11 && tcno.length !== 0) {
+        if (tcno?.length !== 11) {
             setDisabled(true);
-            setTcnoError("TC Kimlik No 11 haneli olmak zorundadır.");
         } else {
             setDisabled(false);
-            setTcnoError("");
         }
     }, [tcno, disabled]);
+
+    const AlertContent = () => {
+        return (
+            <Center marginBottom={10} rounded={8}>
+                <Alert maxW="400" status="error" colorScheme="error">
+                    <VStack space={2} flexShrink={1} w="100%">
+                        <HStack flexShrink={1} space={2} alignItems="center" justifyContent="space-between">
+                            <HStack flexShrink={1} space={4} alignItems="center">
+                                <Alert.Icon />
+                                <Text fontSize="md" fontWeight="medium" color="coolGray.800">
+                                    {errorMessage}
+                                </Text>
+                            </HStack>
+                            <IconButton onPress={() => setErrorMessage("")} variant="unstyled" _focus={{
+                                borderWidth: 0
+                            }} icon={<CloseIcon size="3" />} _icon={{
+                                color: "coolGray.600"
+                            }} />
+                        </HStack>
+                    </VStack>
+                </Alert>
+            </Center>
+        );
+    }
+
+    const HintContent = () => {
+        return (
+            <Box w="14%" alignItems="center">
+                <Popover trigger={triggerProps => {
+                    return <Pressable {...triggerProps}>
+                        <Icon as={<ThemedIcon name={"help_outline"} type="ionicon"/>} size={6}
+                              mr="3" color="muted.400"/>
+                    </Pressable>
+                }}>
+                    <Popover.Content accessibilityLabel="TC-KIMLIK" w="96">
+                        <Popover.Arrow />
+                        <Popover.CloseButton />
+                        <Popover.Header>T.C. Kimlik Numarası 11 Haneli Olmak Zorundadır.</Popover.Header>
+                    </Popover.Content>
+                </Popover>
+            </Box>
+        )
+    }
 
 
     return (
         <Center flex={1} backgroundColor={'white'}>
+            {errorMessage.length > 0 && <AlertContent/>}
             <Box mb="16">
                 <Avatar size="2xl"
                         source={require('../assets/images/akdeniz-universitesi-logo-B017290F9B-seeklogo.com.png')}></Avatar>
@@ -65,13 +115,13 @@ function Login(): React.JSX.Element {
                         md: "25%"
                     }} InputLeftElement={<Icon as={<ThemedIcon name="person" type="ionicon"/>} size={6} ml="2"
                                                color="muted.400"/>}
-                           placeholder="T.C Kimlik No" onChangeText={setTcno}/>
+                           placeholder="T.C Kimlik No" onChangeText={setTcno} InputRightElement={<HintContent/>}/>
                     <Input w={{
                         base: "75%",
                         md: "25%"
-                    }} type={show ? "text" : "password"} InputLeftElement={<Icon as={<ThemedIcon name="key-outline" type="ionicon"/>} size={6} ml="2"
-                                                                                 color="muted.400"/>} InputRightElement={<Pressable onPress={() => setShow(!show)}>
-                        <Icon as={<ThemedIcon name={show ? "eye-outline" : "eye-off-outline"} type="ionicon"/>} size={6}
+                    }} type={showPassword ? "text" : "password"} InputLeftElement={<Icon as={<ThemedIcon name="key-outline" type="ionicon"/>} size={6} ml="2"
+                                                                                 color="muted.400"/>} InputRightElement={<Pressable onPress={() => setShowPasswordPassword(!showPassword)}>
+                        <Icon as={<ThemedIcon name={showPassword ? "eye-outline" : "eye-off-outline"} type="ionicon"/>} size={6}
                               mr="3" color="muted.400"/>
                     </Pressable>} placeholder="Parola" onChangeText={setPassword}/>
                 </Stack>
@@ -80,7 +130,7 @@ function Login(): React.JSX.Element {
                 <Pressable style={{backgroundColor: disabled ? 'gray' : "#4f90ff"}} disabled={disabled} rounded="8"
                            onPress={() => handleLoginSubmit()}>
                     <Box minWidth="45%" minHeight="8">
-                        <Text style={{color: "white", textAlign: "center", fontSize: 20}}>Giriş Yap</Text>
+                        <Text style={{color: "white", textAlign: "center", fontSize: 20, paddingTop:5,}}>Giriş Yap</Text>
                     </Box>
                 </Pressable>
             </Box>
