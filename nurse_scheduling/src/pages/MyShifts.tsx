079668979b6ec23484275f830b52042b39fefee1
Mert Batuhan ÜNVERD <mert.batuhan.unverdi@saritay.com>
2024-05-04 19:10:40 +0300

    feat : MyShifts page and calendar used by endpoint.

--- nurse_scheduling/src/pages/MyShifts.tsx
+++ nurse_scheduling/src/pages/MyShifts.tsx
@@ -1,22 +1,32 @@
 import {Calendar} from 'react-native-big-calendar'
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../contexts/AuthContext.tsx";
+import {useGetShiftsByMonthAndYear} from "../apis/shifts.tsx";
+import {useIsFocused} from "@react-navigation/native";
 
-
+interface ShiftEvent {
+    title: string;
+    start: Date;
+    end: Date;
+}
 export default function MyShifts() {
-    const {nurse} = useContext(AuthContext);
-    const events = [
-        {
-            title: nurse.firstName+" "+nurse.lastName,
-            start: new Date(2024, 2, 18, 0, 0),
-            end: new Date(2024, 2, 18, 8, 0),
-        },
-        {
-            title: nurse.firstName+" "+nurse.lastName,
-            start: new Date(2024, 2, 19, 8, 0),
-            end: new Date(2024, 2, 19, 16, 0),
-        },
-    ]
+    const {credentials,nurse} = useContext(AuthContext);
+    const isFocused = useIsFocused();
+    const month = (new Date().getMonth()+1).toString();
+    const year = new Date().getFullYear().toString();
+    const {shifts} = useGetShiftsByMonthAndYear(nurse.id,month,year,credentials,isFocused);
+    const [events, setEvents] = useState<ShiftEvent[]>([]); // Type annotation for events
+    useEffect(() => {
+        if (shifts) {
+            setEvents(shifts.map((shift) => {
+                return {
+                    title: "",
+                    start: new Date(shift.startDate),
+                    end: new Date(shift.endDate)
+                }
+            }));
+        }
+    },[shifts]);
     return (
         <Calendar mode={"week"} events={events} height={902}/>
     );
